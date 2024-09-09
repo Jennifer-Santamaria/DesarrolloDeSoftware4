900e3ee865e4adb1f86eb3ed4691a0d8443e2b1e
Jennifer-Santamaria <jenniferl.santamariam23@gmail.com>
2024-09-09 11:49:24 -0500

    Actualizacion

--- proyecto3/Calificaciones.cs
+++ proyecto3/Calificaciones.cs
@@ -4,18 +4,17 @@ class Calificaciones
 {
     static void Main(string[] args)
     {
-        // Pedir al usuario que ingrese el promedio numérico del estudiante
+      
         Console.Write("Ingresa el promedio numérico del estudiante (0-100): ");
         int promedio = Convert.ToInt32(Console.ReadLine());
 
-        // Validar que el promedio esté dentro del rango válido (0-100)
+      
         if (promedio < 0 || promedio > 100)
         {
             Console.WriteLine("Por favor ingresa un promedio válido entre 0 y 100.");
         }
         else
         {
-            // Determinar la calificación cualitativa basada en el promedio
             if (promedio >= 90 && promedio <= 100)
             {
                 Console.WriteLine("Calificación: Aprobado con Sobresaliente");
