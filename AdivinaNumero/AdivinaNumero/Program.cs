1560d77c6aae3d87a7d2334ba31d7ce511aab7a1
Jennifer-Santamaria <jenniferl.santamariam23@gmail.com>
2024-09-09 13:33:09 -0500

    Actualizacion

---
+++ AdivinaNumero/AdivinaNumero/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class AdivinaElNumero
+{
+    static void Main(string[] args)
+    {
+       
+        Random random = new Random();
+        int numeroAleatorio = random.Next(1, 101); 
+        int intento;
+        int contadorIntentos = 0;
+
+        Console.WriteLine("La computadora ha generado un número aleatorio entre 1 y 100.");
+
+       
+        do
+        {
+            Console.Write("Ingrese su suposición: ");
+            intento = Convert.ToInt32(Console.ReadLine());
+            contadorIntentos++;
+
+            if (intento < numeroAleatorio)
+            {
+                Console.WriteLine("Demasiado bajo. Inténtalo de nuevo.");
+            }
+            else if (intento > numeroAleatorio)
+            {
+                Console.WriteLine("Demasiado alto. Inténtalo de nuevo.");
+            }
+            else
+            {
+                Console.WriteLine($"¡Correcto! El número era {numeroAleatorio}. Lo adivinaste en {contadorIntentos} intentos.");
+            }
+
+        } while (intento != numeroAleatorio);  
+
+        Console.WriteLine("¡Gracias por jugar!");
+    }
+}
